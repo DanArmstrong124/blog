f2f2a7b192d18e3d96ec1596c69981451dfac5eb
DanArmstrong124 <danielarmstrong124@gmail.com>
2020-07-02 23:30:15 +0000

    Imported Registration to post.views

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import BlogPostForm
+from accounts.views import registration
 
 
 def get_posts(request):
